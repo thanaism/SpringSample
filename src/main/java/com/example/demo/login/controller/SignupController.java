f07d76d683010b010e6e8e7bcb6e63cd0a694963
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-11-28 17:02:19 +0900

    complete chapter 8.3.5 (implement CRUD operations)

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -2,6 +2,9 @@ package com.example.demo.login.controller;
 
 import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,11 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.awt.desktop.SystemEventListener;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Controller
 public class SignupController {
+
+    @Autowired
+    private UserService userService;
 
     private Map<String, String> radioMarriage;
 
@@ -46,6 +53,24 @@ public class SignupController {
         }
 
         System.out.println(form);
+
+        User user = new User();
+
+        user.setUserId(form.getUserId());
+        user.setPassword(form.getPassword());
+        user.setUserName(form.getUserName());
+        user.setBirthday(form.getBirthday());
+        user.setAge(form.getAge());
+        user.setMarriage(form.isMarriage());
+        user.setRole("ROLE_GENERAL");
+
+        boolean result = userService.insert(user);
+
+        if (result == true) {
+            System.out.println("insert成功");
+        } else {
+            System.out.println("insert失敗");
+        }
 
         return "redirect:/login";
     }
